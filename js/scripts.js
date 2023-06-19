640b9aba245fd4ba221dd5249a726a9f5b65bd83
wildfire111 <106965615+wildfire111@users.noreply.github.com>
2023-06-19 17:10:32 +1000

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyUyqpJ-VTUCKVj29Fy97m5hC731-0Ry60hnj8qxDUyELTf5et3IRl0iXgu7MtWkDz0KA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx8MfwOg5GSNLWwMAoSqLD7wCiqlvCrtGmn6LSrua2XOwt0tuNAe5u-W8WU0fUn0WsR9w/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
